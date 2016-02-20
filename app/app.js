3fbe056410f624fd1879ca113d97fbccf5be5c64
Kirk Walker Graves <graves.kirk@gmail.com>
2016-02-19 20:46:17 -0600

    set Firebase URL constant in  app.js file

--- app/app.js
+++ app/app.js
@@ -31,4 +31,4 @@ angular
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://fireslack-app-2016.firebaseio.com/');
